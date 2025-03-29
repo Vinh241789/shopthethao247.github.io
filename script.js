3dbdfc5254aa4a331e6c4e3442d6ad2852a0aa85
shopthethao.github <Vinh241789@gmail.com>
2025-03-29 07:54:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('loginBtn').addEventListener('click', function() {
+    // Khi nút được nhấn, chuyển đến trang web khác
+    window.location.href = 'https://www.example.com'; // URL của trang cần chuyển đến
+  });
+  
